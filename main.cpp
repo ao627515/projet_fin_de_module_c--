329862e70ed2bf2c09248ea10183d2be10693b6a
ao627515 <abdo.ouedraogo.03@gmail.com>
2024-01-10 10:08:38 +0000

    definition methode augmenter

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ int main(int argc, char *argv[])
 {
 
     try {
-           CEmployer emp("Ouedraogo", "Abdoul", "PDF", CEmployer::fonctionnaire, "Ouagadougou", "26/11/2000", "10/1/2024", 50000.0);
+           CEmployer emp("Ouedraogo", "Abdoul", "PDF", CEmployer::fonctionnaire, "Ouagadougou", "26/11/2000", "10/1/2024", 50000);
 
-        cout << emp.estFonctionnaire() << endl;
-        cout << emp.estAuxiliaire() << endl;
-
+        cout << "salaire : " << emp.calculerSalaire() << endl;
+        cout << "salaire : " << emp.calculerSalaire(10) << endl;
+        cout << "salaire : " << emp.calculerSalaire(100) << endl;
+        cout << "salaire : " << emp.calculerSalaire(150) << endl;
     } catch (std::invalid_argument e) {
         cout << "Erreur : " << e.what();
     }
